6f42da3b1ba37b332871b6e1369de3bcbf39ae73
Lucas Nunes <lucas.jlnunes@gmail.com>
2025-02-09 15:21:22 -0300

    I have to turn everything below the swiper slide into a reusable component

--- client/src/components/Background.tsx
+++ client/src/components/Background.tsx
@@ -1,24 +1,68 @@
-import Image from 'next/image'
-import React from 'react'
-import { boot } from '../../public/images'
-import ShopNow from './ShopNow'
+"use client";
+import Image from "next/image";
+import React from "react";
+import { boot } from "../../public/images";
+import ShopNow from "./ShopNow";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Autoplay, Navigation} from "swiper/modules";
+// Import Swiper styles
 
+
+import "swiper/css";
 const Background = () => {
+  SwiperCore.use([Navigation, Autoplay]);
   return (
-    <div className='boot-div'>
-      <Image src={boot} alt='boot' className='boot-img'  />
-      <div className='boot-text'>
-        <span className='z-10'>Welcome to our soccer club</span>
-        <div className='boot-title'>
-          <h1>Champions <div className='inline mark'>rise,</div> heroes are made</h1>
+    <Swiper spaceBetween={50} slidesPerView={1} autoHeight={true}
+      navigation={{
+        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next"
+      }}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      loop={true}
+    >
+      <SwiperSlide>
+        <div className="boot-div">
+          <Image src={boot} alt="boot" className="boot-img" />
+          <div className="boot-text">
+            <span className="z-10">Welcome to our soccer club</span>
+            <div className="boot-title">
+              <h1>
+                Champions <div className="inline mark">rise,</div> heroes are
+                made
+              </h1>
+            </div>
+            <div className="flex justify-center gap-5">
+              <ShopNow text={"Shop Now"} bg="#AF101A" textColor="white" />
+              <ShopNow text={"See Collection"} bg="#ffffff" textColor="black" />
+            </div>
+          </div>
         </div>
-        <div className='flex justify-center gap-5'>
-          <ShopNow text={"Shop Now"} bg='#AF101A' textColor='white' />
-          <ShopNow text={"See Collection"} bg='#ffffff' textColor='black' />
-        </div>
-      </div>
-    </div>
-  )
-}
+      </SwiperSlide>
 
-export default Background
+      <SwiperSlide>
+        <div className="boot-div">
+          <Image src={boot} alt="boot" className="boot-img" />
+          <div className="boot-text">
+            <span className="z-10">Welcome to our soccer club</span>
+            <div className="boot-title">
+              <h1>
+                Champions <div className="inline mark">rise,</div> heroes are
+                made
+              </h1>
+            </div>
+            <div className="flex justify-center gap-5">
+              <ShopNow text={"Shop Now"} bg="#AF101A" textColor="white" />
+              <ShopNow text={"See Collection"} bg="#ffffff" textColor="black" />
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <div className="swiper-button-prev">a</div>
+      <div className="swiper-button-next">n</div>
+    </Swiper>
+  );
+};
+
+export default Background;
